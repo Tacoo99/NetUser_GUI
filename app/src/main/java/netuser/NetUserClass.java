4fe998afa98edf97c252590757c7e29855f3270c
Wojtek <koziolwojciech99@gmail.com>
2022-09-04 20:58:21 +0200

    Added function - changing user password

--- app/src/main/java/netuser/NetUserClass.java
+++ app/src/main/java/netuser/NetUserClass.java
@@ -2,10 +2,7 @@ package netuser;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
@@ -13,6 +10,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Optional;
 
 public class NetUserClass {
 
@@ -38,13 +36,13 @@ public class NetUserClass {
     private PasswordField newPassword;
 
     @FXML
+    private PasswordField repeatPassword;
+
+    @FXML
     private TextField passwordExpires;
 
     @FXML
     private Button searchBtn;
-
-    @FXML
-    private Button unlockBtn;
 
     @FXML
     private TextField username;
@@ -65,8 +63,20 @@ public class NetUserClass {
         alert.setTitle(title);
         alert.setHeaderText(header);
         alert.setContentText(content);
+        alert.initOwner(searchBtn.getScene().getWindow());
 
         alert.showAndWait();
+    }
+
+    boolean myConfirmation(String title, String header, String content){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.initOwner(searchBtn.getScene().getWindow());
+
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.get() == ButtonType.OK;
     }
 
     String formatString(String text, int number) {
@@ -82,14 +92,6 @@ public class NetUserClass {
         return result.toString();
     }
 
-    boolean check(String text, String first, String second) {
-        if (text.contains(first) || (text.contains(second))) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     void GetInformation(Process p) {
         try {
             BufferedReader output_reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -103,6 +105,12 @@ public class NetUserClass {
 
                 if (i == 6) {
                     activeAccount.setText(formatString(output, 3));
+                    if(activeAccount.getText().matches("(.*)Tak(.*)")){
+                        activeAccount.setStyle("-fx-border-color: green");
+                    }
+                    else if(activeAccount.getText().matches("(.*)Nie(.*)")){
+                        activeAccount.setStyle("-fx-border-color: tomato");
+                    }
                 }
 
                 if (i == 7) {
@@ -121,19 +129,24 @@ public class NetUserClass {
                     lastLogin.setText(formatString(output, 2));
                 }
             }
+
+            int r = p.waitFor(); // Let the process finish.
+            if (r != 0) { // Error
+                myAlert(Alert.AlertType.ERROR, "Wystąpił błąd", "Wystąpił problem z danymi wejściowymi", "Nie znaleziono użytkownika " + getUsername());
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+
+
     }
 
     @FXML
-    void Search(ActionEvent event) {
+    void Search() {
         if (checkUsername(username)) {
-
             try {
                 ProcessBuilder build_test = new ProcessBuilder(
                         "cmd.exe", "/c", "net user " + getUsername());
-                build_test.redirectErrorStream(true);
                 Process p = build_test.start();
                 GetInformation(p);
             } catch (IOException e) {
@@ -145,19 +158,88 @@ public class NetUserClass {
         }
     }
 
-    @FXML
-    void changePassword(ActionEvent event) {
-
+    void clearPasswords(){
+        for (PasswordField passwordField : Arrays.asList(newPassword, repeatPassword)) {
+            passwordField.clear();
+        }
     }
 
     @FXML
-    void txtFieldKeyReleased(KeyEvent event) {
+    void changePassword() {
+        String password = newPassword.getText();
+        String repeatedPassword = repeatPassword.getText();
+
+        if(password.equals(repeatedPassword)){
+            if (checkUsername(username)) {
+                try {
+                    ProcessBuilder build_test = new ProcessBuilder(
+                            "cmd.exe", "/c", "net user " + getUsername() + " " + password);
+                    Process p = build_test.start();
+                    int r = p.waitFor(); // Let the process finish.
+                    if (r != 0) { // Error
+                        myAlert(Alert.AlertType.ERROR, "Wystąpił błąd", "Wystąpił problem ze zmianą hasła", "Hasło użytkownika " + getUsername() + " nie zostało pomyślnie zmienione");
+                        clearPasswords();
+                    }
+                    else{
+                        myAlert(Alert.AlertType.INFORMATION, "Operacja powiodła się", "Hasło zostało ustawione", "Hasło użytkownika " + getUsername() + " zostało pomyślnie zmienione");
+                        clearPasswords();
+                    }
+                } catch (IOException | InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            } else {
+                myAlert(Alert.AlertType.ERROR, "Wystąpił błąd", "Brak wszystkich wymaganych danych", "Podaj nazwę użytkownika");
+            }
+        }
+        else{
+            myAlert(Alert.AlertType.ERROR, "Wystąpił błąd", "Wprowadzone dane są niepoprawne", "Hasła różnią się od siebie");
+        }
 
     }
 
-    @FXML
-    void unclockAccount(ActionEvent event) {
+    void manageAccount(String operation){
 
+        if (checkUsername(username)) {
+            try {
+                ProcessBuilder build_test = new ProcessBuilder(
+                        "cmd.exe", "/c", "net user " + getUsername() + " /ACTIVE:"+ operation );
+                Process p = build_test.start();
+                int r = p.waitFor(); // Let the process finish.
+                if (r != 0) { // Error
+                    myAlert(Alert.AlertType.ERROR, "Wystąpił błąd", "Wystąpił problem z oblokowaniem/zablokowaniem konta", "Nie można było uzyskać dostępu do konta " + getUsername());
+                }
+                else{
+                    searchBtn.fire();
+                }
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        } else {
+            myAlert(Alert.AlertType.ERROR, "Wystąpił błąd", "Brak wszystkich wymaganych danych", "Podaj nazwę użytkownika");
+        }
+    }
+
+    @FXML
+    void unclockAccount() {
+        manageAccount("YES");
+    }
+
+    @FXML
+    void blockAccount(){
+        manageAccount("NO");
+    }
+
+    @FXML
+    void setDefaultPassword(){
+        if (checkUsername(username)) {
+            myConfirmation("Potwierdź operację", "Czy na pewno chcesz wykonać tą akcję?", "Hasło do konta użytkownika " + getUsername() +" zmieni się na standardowe SD");
+        }
+
+        else{
+
+        }
     }
 
     @FXML
@@ -168,25 +250,25 @@ public class NetUserClass {
     }
 
     @FXML
-    void aboutAuthor(ActionEvent event) {
+    void aboutAuthor() {
         myAlert(Alert.AlertType.INFORMATION, "Autor programu", "Informacja o autorze programu", "Autor: Wojciech Kozioł\nFirma: Fujitsu");
     }
 
     @FXML
-    void aboutProgram(ActionEvent event) {
+    void aboutProgram() {
         myAlert(Alert.AlertType.INFORMATION, "O programie", "Informacja o programie", "Program służy do wyświetlania informacji o użytkowniku których brakuje w przystawce Active Directory\n" +
                 "Jest on ciągle w rozwoju, aktualnie posiada podstawowe funkcje - wyświetlania informacji, odblokowania konta oraz zmiany hasła.");
     }
 
     @FXML
-    void clearFields(ActionEvent event) {
-        for (TextField textField : Arrays.asList(username, fullName, activeAccount, accountExpires, lastChange, passwordExpires, lastLogin)) {
+    void clearFields() {
+        for (TextField textField : Arrays.asList(username, fullName, activeAccount, accountExpires, lastChange, passwordExpires, lastLogin, newPassword, repeatPassword)) {
             textField.clear();
         }
     }
 
     @FXML
-    void exit(ActionEvent event) {
+    void exit() {
         System.exit(0);
     }
 
